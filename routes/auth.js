6ae473069c7f870caa9336c67c11754269b1fd97
Frederico Amaral <fredamaral@gmail.com>
2021-03-08 22:51:38 -0300

    Novo Push

--- routes/auth.js
+++ routes/auth.js
@@ -4,22 +4,34 @@ var router = express.Router();
 
 const urL_validacao_token = "https://oauth-redirect.googleusercontent.com/r/YOUR_PROJECT_ID?code=AUTHORIZATION_CODE&state=STATE_STRING&scope=REQUESTED_SCOPES&response_type=code&user_locale=LOCALE"
 
+// router.get('/', function (req, res, next) {
+//     var dados = getParams(req);
+
+//     var url = urL_validacao_token
+//         .replace("YOUR_PROJECT_ID", "")
+//         .replace("AUTHORIZATION_CODE", "")
+//         .replace("STATE_STRING", "")
+//         .replace("REQUESTED_SCOPES", "")
+//         .replace("LOCALE", "");
+
+//     res.send({
+//         "token_type": "Bearer",
+//         "access_token": "asdaSD",
+//         "refresh_token": "asdASDasd",
+//         "expires_in": 600
+//     });
+// });
+
 router.get('/', function (req, res, next) {
-    var dados = getParams(req);
-
-    var url = urL_validacao_token
-        .replace("YOUR_PROJECT_ID", "")
-        .replace("AUTHORIZATION_CODE", "")
-        .replace("STATE_STRING", "")
-        .replace("REQUESTED_SCOPES", "")
-        .replace("LOCALE", "");
-
-    res.send({
-        "token_type": "Bearer",
-        "access_token": "asdaSD",
-        "refresh_token": "asdASDasd",
-        "expires_in": 600
-    });
+    res.send(`<html>
+    <body>
+    <form action="/login" method="post">
+    <input type="hidden" name="response_url" value="${req.query.response_url}" />
+    <button type="submit" style="font-size:14pt">Link this service to Google</button>
+    </form>
+    </body>
+    </html>
+    `);
 });
 
 function validateToken(dados) {
